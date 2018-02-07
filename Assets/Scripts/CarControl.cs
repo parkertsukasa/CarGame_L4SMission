f534309537429afa777c742e9c1c515b0cc3b0fc
TsukasaSumizono <tsukasa@M-TsukasaSumizono.local>
2018-02-07 13:32:03 +0900

    make:GameOver　Dush

--- Assets/Scripts/CarControl.cs
+++ Assets/Scripts/CarControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CarControl : MonoBehaviour
 { 
@@ -9,22 +10,27 @@ public class CarControl : MonoBehaviour
 
 	private Rigidbody rig;
 
-	private GameObject model;
+	private GameObject effect;
 
-	public float max_speed;
-	private Image coingauge;
 	private int coin = 0;
+	private float cointimer = 0.0f;
 
+	private bool dush;
 
+	public GameObject explosion;
+
+	//---------- ステータス ---------
+	public float max_speed;
+	public float accelaration;
 
 	// Use this for initialization
 	void Start()
 	{
 		Input.gyro.enabled = true;//入力のジャイロを有効にする。
-
-		coingauge = GameObject.Find("CoinGauge").GetComponent<Image>();
+	
 		rig = GetComponent<Rigidbody>();
-		model = GameObject.Find("CarModel");
+		effect = GameObject.Find("Effect");
+		effect.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -48,7 +54,12 @@ public class CarControl : MonoBehaviour
 
 		if(rig.velocity.z < max_speed)
 		{
-			rig.velocity += Vector3.forward * 1.0f;
+			rig.velocity += Vector3.forward * accelaration;
+		}
+
+		if(rig.velocity.z > max_speed)
+		{
+			rig.velocity -= Vector3.forward * accelaration * 2;
 		}
 	}
 
@@ -93,25 +104,28 @@ public class CarControl : MonoBehaviour
 				break;
 
 			case hundle.right:
+				if (rig.velocity.z > 3.0f)
+				{
+					//----- はじまで行かないように右移動 -----
+					if (transform.position.x < 9.0f)
+						transform.Translate(Vector3.right * gyro.z * turn);
 
-				//----- はじまで行かないように右移動 -----
-				if(transform.position.x < 9.0f)
-					transform.Translate(Vector3.right * gyro.z * turn);
-
-				if (transform.position.x > 9.0f)
-					transform.position = new Vector3(9.0f, transform.position.y, transform.position.z);
-
+					if (transform.position.x > 9.0f)
+						transform.position = new Vector3(9.0f, transform.position.y, transform.position.z);
+				}
 				
 				break;
 
 			case hundle.left:
+				if (rig.velocity.z > 3.0f)
+				{
+					//----- はじまで行かないように左移動 -----
+					if (transform.position.x > -9.0f)
+						transform.Translate(Vector3.right * gyro.z * turn);
 
-				//----- はじまで行かないように左移動 -----
-				if (transform.position.x > -9.0f)
-					transform.Translate(Vector3.right * gyro.z * turn);
-
-				if (transform.position.x < -9.0f)
-					transform.position = new Vector3(-9.0f, transform.position.y, transform.position.z);
+					if (transform.position.x < -9.0f)
+						transform.position = new Vector3(-9.0f, transform.position.y, transform.position.z);
+				}
 
 				break;
 		}
@@ -119,7 +133,27 @@ public class CarControl : MonoBehaviour
 
 	void CoinManage()
 	{
+		Image coingauge = GameObject.Find("CoinGauge").GetComponent<Image>();
 		coingauge.fillAmount = (float)coin / 10.0f;
+
+		Image button = GameObject.Find("DushButton").GetComponent<Image>();
+		Text text = GameObject.Find("DushText").GetComponent<Text>();
+
+		//----- コインが溜まった時の処理 -----
+		if (coin == 10)
+		{
+			float sin = (Mathf.Sin(cointimer) + 1.0f) / 2;
+			cointimer += Time.deltaTime * 2;
+			text.text = "超絶\n加速";
+			button.color = new Color(1.0f, sin, sin);
+			text.color = new Color(1.0f, sin, sin);
+		}
+		else
+		{
+			button.color = new Color(1.0f, 1.0f, 1.0f);
+			text.color = new Color(1.0f, 1.0f, 1.0f);
+			text.text = "";
+		}
 	}
 
 	void OnTriggerEnter(Collider col)
@@ -133,6 +167,47 @@ public class CarControl : MonoBehaviour
 			Destroy(col.gameObject);
 
 		}
+
+		//----- 牛に当たった時の処理 -----
+		if (col.gameObject.tag == "Walker")
+		{
+			if (!dush)
+			{
+				Instantiate(explosion, transform.position, transform.rotation);
+				Invoke("GameOver", 1.0f);		
+			}
+		}
 	}
+
+	void GameOver ()
+	{
+		SceneManager.LoadScene("Stage");
+	}
+
+	//----- 超絶加速ボタンが押された時の処理 -----
+	public void Dush()
+	{
+		if (!dush && coin == 10)
+		{
+			StartCoroutine("OneSecDush");
+			coin = 0;
+		}
+	}
+
+	IEnumerator OneSecDush ()
+	{
+		max_speed *= 2;
+		accelaration *= 2;
+		dush = true;
+		effect.SetActive(true);
+
+		yield return new WaitForSeconds(5.0f);
+
+		max_speed /= 2;
+		accelaration /= 2;
+		dush = false;
+		effect.SetActive(false);
+	}
+
 
 }
--- Assets/Scripts/RoadCreate.cs
+++ Assets/Scripts/RoadCreate.cs
@@ -9,6 +9,8 @@ public class RoadCreate : MonoBehaviour
 
 	public GameObject coin;
 
+	public GameObject walker;
+
 	// Use this for initialization
 	void Start()
 	{
@@ -17,6 +19,7 @@ public class RoadCreate : MonoBehaviour
 		{
 			Instantiate(road, new Vector3(0, 0, i * 60), Quaternion.Euler(-90, 0, 0));
 			Instantiate(coin, new Vector3(Random.Range(-9.0f, 9.0f), 12, i * Random.Range(14, 16)), Quaternion.identity);
+			Instantiate(walker, new Vector3(Random.Range(-9.0f, 9.0f), 10.5f, (i + 5) * Random.Range(14, 16)), Quaternion.identity);
 		}
 
 	}
